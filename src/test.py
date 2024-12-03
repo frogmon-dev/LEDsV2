3e0d713b32acf9b84560d0e74d095d60092b25c1
junghuicheol <dyujhc@gmail.com>
2024-12-03 21:38:10 +0900

    add fontsss

--- src/test.py
+++ src/test.py
@@ -37,14 +37,14 @@ try:
         matrix.Clear()  # 화면 초기화
         # 이미지를 매트릭스에 출력
         matrix.SetImage(imgTemp, 0, 0)  # 이미지 위치 (0, 0)
-        graphics.DrawText(matrix, font, 16, 10, temp_text_color, '온도 :')  # 텍스트 출력
+        graphics.DrawText(matrix, font, 16, 10, temp_text_color, '온도:')  # 텍스트 출력
         
-        graphics.DrawText(matrix, font, 40, 10, temp_text_color, '0°C')  
+        graphics.DrawText(matrix, font, 40, 10, temp_text_color, '%2d°C'%9)  
         
         matrix.SetImage(imgHumi, 0, 16)  # 이미지 위치 (0, 0)
-        graphics.DrawText(matrix, font, 16, 28, humi_text_color, '습도 :')  # 텍스트 출력
+        graphics.DrawText(matrix, font, 16, 28, humi_text_color, '습도:')  # 텍스트 출력
         
-        graphics.DrawText(matrix, font, 40, 28, humi_text_color, '90%')  # 텍스트 출력
+        graphics.DrawText(matrix, font, 40, 28, humi_text_color, '%2d%'%9)  # 텍스트 출력
         
         #len = graphics.DrawText(matrix, font, pos, 16, text_color, message)  # 텍스트 출력
         #pos -= 1  # 텍스트 이동 (왼쪽으로 스크롤)
